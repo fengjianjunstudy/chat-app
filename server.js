2e3061b4b22417226b4050904349c3053df5f6e3
fjj_321 <fjj_321@126.com>
2017-08-24 23:38:28 +0800

    create server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var cache = {};
+var chatServer = require('./lib/chat_server');
 
 
 function send404(res){
@@ -15,12 +16,10 @@ function sendFile(res,filePath,fileContents){
     res.end(fileContents);
 }
 function serverStatic(res,cache,absPath){
-    console.log(absPath)
     if(cache[absPath]){
         sendFile(res,absPath,cache[absPath]);
     }else{
         fs.exists(absPath, function (exists) {
-            console.log(exists)
             if(exists){
                 fs.readFile(absPath,function(err,data){
                     if(err){
@@ -47,6 +46,7 @@ var server = http.createServer(function(req,res){
     serverStatic(res,cache,absPath)
 })
 
-server.listen(3000,function(){
-    console.log('server listening on port 3000')
-})
+//server.listen(3000,function(){
+//    console.log('server listening on port 3000')
+//})
+chatServer.listen(server);
